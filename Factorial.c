f8c4b1d2b76c489009afb7051c2503fb3a6df198
i447 <68431541+i447@users.noreply.github.com>
2020-09-22 02:51:01 +0530

    Update Factorial.c
    To Find Factorial of Given Number

--- Factorial.c
+++ Factorial.c
@@ -3,7 +3,8 @@ int main()
 {
 	int n,i;
 	long long int a=1;
-	printf("Enter the Number ");
+	printf("\nTo Find Factorial of the Given Number");
+	printf("\nEnter the Number ");
 	scanf("%d",&n);
 	if(n<0)
 	printf("Factorial does not Exist");
